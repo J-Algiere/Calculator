64967ec5c4f0c17e841eaf25cb3b3f5a5ccc8cad
J-Algiere <algiere.jarad@gmail.com>
2025-07-02 20:58:12 -0500

    Commit 3

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 # Build a calculator program
+import sys
 
 def main():
-    equation = userinput()
-    print(eval(equation))
-
-
+    while True:
+        equation = userinput()
+        try:
+            print(eval(equation))
+            sys.exit()
+        except ZeroDivisionError:
+            print("Can not divide by zero")
+        except NameError:
+            print("Invalid input")
+        except SyntaxError:
+            print("Invalid input")
 
 # get user input for numbers to calculate
 def userinput():
-    x = input("Enter a math problem: ")
-    return x
-
-
-
-main()
+        x = input("Enter a math problem: ")
+        return x
+if __name__ == '__main__':
+    main()
