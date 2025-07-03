2066c89d7704ce615b5e0e0c0b801e23973a7a84
J-Algiere <algiere.jarad@gmail.com>
2025-07-02 20:25:18 -0500

    Commit 2

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # Build a calculator program
+
+def main():
+    equation = userinput()
+    print(eval(equation))
+
+
+
+# get user input for numbers to calculate
+def userinput():
+    x = input("Enter a math problem: ")
+    return x
+
+
+
+main()
